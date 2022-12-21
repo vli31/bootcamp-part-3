4f9cd3db540acd55f93ab1ecaa98e9a622287301
Victoria <victoriali2012@gmail.com>
2022-12-20 22:49:41 -0500

    react router

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import CardEditor from './CardEditor';
 import CardViewer from './CardViewer';
 
+import { Switch, Route } from 'react-router-dom';
+
 class App extends React.Component {
   constructor(props) {
     super(props);
@@ -24,24 +26,22 @@ class App extends React.Component {
     cards.splice(index, 1);
     this.setState({ cards });
   };
-
-  switchMode = () => this.setState({ editor: !this.state.editor });
   
   render() {
-    if (this.state.editor) {
-      return (
-        <CardEditor
-          addCard={this.addCard}
-          cards={this.state.cards}
-          deleteCard={this.deleteCard}
-          switchMode={this.switchMode}
-        />
-      );
-    } else {
-      return (
-        <CardViewer cards={this.state.cards} switchMode={this.switchMode} />
-      );
-    }
+    return (
+      <Switch>
+        <Route exact path="/editor">
+          <CardEditor
+            addCard={this.addCard}
+            cards={this.state.cards}
+            deleteCard={this.deleteCard}
+          />
+        </Route>
+        <Route exact path="/viewer">
+          <CardViewer cards={this.state.cards} />
+        </Route>
+      </Switch>
+    );
   }
 }
 
